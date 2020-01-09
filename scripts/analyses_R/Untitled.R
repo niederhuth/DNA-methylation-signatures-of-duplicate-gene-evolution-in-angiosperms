cfc66f98c39842d993eee246258d62667713aa73
Niederhuth <niederhu@msu.edu>
2020-01-09 11:58:00 -0500

    R

--- scripts/analyses_R/Untitled.R
+++ scripts/analyses_R/Untitled.R
@@ -1,6 +1,8 @@
+library(ggplot2)
+
 species <- read.csv('../misc/genomes.csv',header=T)
 
-for( a in species$species){
+for( a in species[species$methylC == "yes",]$species){
   path1=paste(a,"/dupgen/results-unique/classified_genes.tsv",sep="")
   path2=paste(a,"/methylpy/results/",a,"_classified_genes.tsv",sep="")
   df1 <- read.table(path1,header=TRUE,sep="\t")
@@ -23,21 +25,28 @@ for( a in species$species){
 }
 
 #Test
-df1 <- read.table("classified_genes.tsv",header=TRUE,sep="\t")
-df2 <- read.table("Athaliana_classified_genes.tsv",header=TRUE,sep="\t")
-df3 <- merge(df1,df2,by.x="Feature",by.y="Feature")
-df4 <- as.data.frame(table(df3[2:3]))
-df4$Perc <- NA
-df4$Perc2 <- NA
+species="Athaliana"
 
-for(i in df4$Duplication){
-  x = sum(df4[df4$Duplication==i,]$Freq)
-  df4[df4$Duplication==i,]$Perc = df4[df4$Duplication==i,]$Freq/x
-}
-
-for(i in df4$Classification){
-  x = sum(df4[df4$Classification==i,]$Freq)
-  df4[df4$Classification==i,]$Perc2 = df4[df4$Classification==i,]$Freq/x
+for( a in species){
+  path1=paste(a,"/dupgen/results-unique/classified_genes.tsv",sep="")
+  path2=paste(a,"/methylpy/results/",a,"_classified_genes.tsv",sep="")
+  df1 <- read.table(path1,header=TRUE,sep="\t")
+  df2 <- read.table(path2,header=TRUE,sep="\t")[,c(1,23)]
+  df3 <- merge(df1,df2,by.x="Feature",by.y="Feature")
+  df4 <- as.data.frame(table(df3[2:3]))
+  df4$Perc <- NA
+  df4$Perc2 <- NA
+  
+  for(i in df4$Duplication){
+    x = sum(df4[df4$Duplication==i,]$Freq)
+    df4[df4$Duplication==i,]$Perc = df4[df4$Duplication==i,]$Freq/x
+  }
+  
+  for(i in df4$Classification){
+    x = sum(df4[df4$Classification==i,]$Freq)
+    df4[df4$Classification==i,]$Perc2 = df4[df4$Classification==i,]$Freq/x
+  }
+  
 }
 
 p <- ggplot(df4) + 
