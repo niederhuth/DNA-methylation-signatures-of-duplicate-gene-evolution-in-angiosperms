e75791e79e1dbc586b29a838619855b95700dda8
Niederhuth <niederhu@msu.edu>
2020-01-21 14:06:28 -0500

    TE table

---
+++ scripts/analyses_sh/TE_intersections.sh
@@ -0,0 +1,25 @@
+#!/bin/bash --login
+#SBATCH --time=3:59:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1GB
+#SBATCH --job-name TE_intersections
+#SBATCH --output=job_reports/%x-%j.SLURMout
+
+cd $PBS_O_WORKDIR
+export PATH="$HOME/miniconda3/envs/gene-duplication/bin:$PATH"
+export LD_LIBRARY_PATH="$HOME/miniconda3/envs/gene-duplication/lib:$LD_LIBRARY_PATH"
+
+#Set tmp directories
+export TMPDIR=$PBS_O_WORKDIR
+export TMP=$PBS_O_WORKDIR
+export TEMP=$PBS_O_WORKDIR
+
+#variables
+sample=$(pwd | sed s/.*data\\/// | sed s/\\/.*//)
+
+#get total weighted mC
+echo "TE intersections for $sample"
+python ../../../scripts/analyses_py/TE_intersections.py $sample
+
