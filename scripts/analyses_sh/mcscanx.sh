11f0604ff2a81d83d096faf6d91137c8d94ddbad
Niederhuth <niederhu@msu.edu>
2020-05-28 16:57:48 -0400

    stuff

--- scripts/analyses_sh/mcscanx.sh
+++ scripts/analyses_sh/mcscanx.sh
@@ -3,69 +3,48 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=50GB
-#SBATCH --job-name MCScanX-transposed
+#SBATCH --mem=1GB
+#SBATCH --job-name MCScanX
 #SBATCH --output=job_reports/%x-%j.SLURMout
 
 cd $PBS_O_WORKDIR
+SCRIPTS=$(pwd | sed s/methylation.*/methylation\\/scripts/)
 export PATH="$HOME/miniconda3/envs/gene-duplication/bin:$PATH"
+export PATH="$SCRIPTS/DupGen_finder:$PATH"
 export LD_LIBRARY_PATH="$HOME/miniconda3/envs/gene-duplication/lib:$LD_LIBRARY_PATH"
 
 #Set Variables
 sample=$(pwd | sed s/^.*\\///)
-species=$(awk -v FS="," -v a="$sample" '$1 == a' ../../misc/genomes.csv | cut -d ',' -f 6)
-outgroup_list=$(echo $species | tr ' ' ',')
-epochs=$(awk -v FS="," -v a="$sample" '$1 == a' ../../misc/genomes.csv | cut -d ',' -f 7)
+outgroup=$(awk -v FS="," -v a="$sample" '$1 == a' ../../misc/genomes.csv | cut -d ',' -f 6)
 
-#Run MCScanX-transposed
-mkdir mcscanx
-mkdir mcscanx/data
-echo "Sample: $sample"
-echo "Outgroups: $species"
-echo "Epochs: $epochs"
-echo "Copying $sample data"
-cp ref/mcscanx/"$sample".gff mcscanx/data/"$sample".gff
-cp diamond/"$sample"-"$sample".m8 mcscanx/data/"$sample".blast
-for i in $species
+#Run MCScanX
+cd dupgen
+mkdir collinearity
+
+for i in $outgroup
 do
-	echo "Copying $i data"
-	cat ref/mcscanx/"$sample".gff ../"$i"/ref/mcscanx/"$i".gff > mcscanx/data/"$sample"_"$i".gff
-	cat diamond/"$sample"-"$i".m8 ../"$i"/diamond/"$i"-"$sample".m8 > mcscanx/data/"$sample"_"$i".blast
+	echo "Outgroup: $i"
+	cat ../ref/mcscanx/"$sample".gff ../../"$i"/ref/mcscanx/"$i".gff > data/"$sample"_"$i".gff
+	cp ../diamond/"$sample"-"$i".m8 data/"$sample"_"$i".blast
+
+	MCScanX data/"$sample"_"$i" collinearity/"$sample"_"$i"
 done
-cd mcscanx
-echo "Running MCScanX"
-cp ../../../scripts/MCScanX-transposed.tar.gz ./
-tar -xjvf MCScanX-transposed.tar.gz
-mv MCScanX-transposed/* ./
-rmdir MCScanX-transposed
-perl MCScanX-transposed.pl \
-	-i data \
-	-t "$sample" \
-	-c "$outgroup_list" \
-	-o results \
-	-x "$epochs"
 
-rm *
-
-cd results
-head -1 "$sample".transposed.pairs | \
-awk -v OFS="\t" '{print $0,"epoch","epoch_species"}' >"$sample".transposed_epoch.pairs
-count=1
-for i in $species
+cd collinearity
+echo "$sample $outgroup" | tr ' ' ',' > collinearity.csv
+cat ../data/"$sample".gff | cut -f2 | while read line
 do
-	if [ $count -lt 2 ]
-	then
-		sed '1d' "$sample".transposed_after_"$i".pairs | \
-		awk -v b=$count -v c=$i -v OFS="\t" '{print $0,b,c}' >> "$sample".transposed_epoch.pairs
-		count=`expr $count + 1`
-		a=$i
-	else
-				
-		sed '1d' "$sample".transposed_between_"$a"_"$i".pairs | \
-		awk -v b=$count -v c=$i -v OFS="\t" '{print $0,b,c}' >> "$sample".transposed_epoch.pairs
-		count=`expr $count + 1`
-		a=$i
-	fi
+	a="$line"
+	for i in $outgroup
+	do
+		if grep -q $line "$sample"_"$i".collinearity
+		then
+			a=$(echo "$a,$i")
+		else
+			a=$(echo "$a,")
+		fi
+	done
+	echo $a >> collinearity.csv
 done
 
 echo "Done"
