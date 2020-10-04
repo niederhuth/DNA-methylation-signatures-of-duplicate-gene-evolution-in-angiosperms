cc683500c5f363d9766b5e580eb41f9afee06b16
Niederhuth <niederhu@msu.edu>
2020-10-04 15:02:57 -0400

    stuff

--- scripts/analyses_sh/orthofinder.sh
+++ scripts/analyses_sh/orthofinder.sh
@@ -1,9 +1,9 @@
 #!/bin/bash --login
-#SBATCH --time=48:00:00
+#SBATCH --time=96:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=41
-#SBATCH --mem=50GB
+#SBATCH --cpus-per-task=128
+#SBATCH --mem=128GB
 #SBATCH --job-name orthofinder
 #SBATCH --output=%x-%j.SLURMout
 
@@ -12,8 +12,9 @@ export PATH="$HOME/miniconda3/envs/orthofinder/bin:$PATH"
 export LD_LIBRARY_PATH="$HOME/miniconda3/envs/orthofinder/lib:$LD_LIBRARY_PATH"
 
 #Set Variables
-threads=40
 species=$(cut -d ',' -f1 ../../misc/genomes.csv | sed '1d' | tr '\n' ' ')
+threads=128
+threads2=4
 
 #Run OrthoFinder
 echo "Copying sequence files"
@@ -26,7 +27,7 @@ done
 echo "Running OrthoFinder"
 orthofinder \
 	-t $threads \
-	-a 1 \
+	-a $threads2 \
 	-M dendroblast \
 	-S diamond \
 	-o orthofinder \
@@ -38,6 +39,15 @@ do
 	og=$(echo $line | cut -d ' ' -f1 | sed s/\\:$//)
 	echo $line | tr ' ' '\n' | sed '1d' | awk -v OFS='\t' -v a=$og '{print $0,a}' >> orthogroup_list.tsv
 done < orthofinder/*/Orthogroups/Orthogroups.txt
+
+echo "Get orthogroups for each species & gene"
+for i in $species
+do
+	echo $i
+	cut -f2 ../"$i"/ref/mcscanx/"$i".gff > tmp
+	fgrep -f tmp orthogroup_list.tsv > ../"$i"/ref/mcscanx/"$i"_orthogroups.tsv
+done
+rm tmp
 
 echo "Done"
 
