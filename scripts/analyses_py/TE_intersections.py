4267d1e164e21f4f0cf6e01166e558ffd4511a78
Niederhuth <niederhu@msu.edu>
2021-02-22 19:28:37 -0500

    stuff

--- scripts/analyses_py/TE_intersections.py
+++ scripts/analyses_py/TE_intersections.py
@@ -4,14 +4,13 @@ import pandas as pd
 import pybedtools as pbt
 import gffutils
 
-
 functionsfile = '../../scripts/functions.py'
 sys.path.append(os.path.dirname(os.path.expanduser(functionsfile)))
 
 import functions
 
 #Set Variables
-updown_stream=[0,100,250,500,750,1000,1500,2000,3000,4000,5000,7500,10000]
+updown_stream=[100,250,500,750,1000,1500,2000,3000,4000,5000,7500,10000]
 annotations='ref/annotations/'+sys.argv[1]+'.gff'
 TEs='ref/annotations/'+sys.argv[1]+'-TEanno.gff'
 genome_file='ref/'+sys.argv[1]+'.fa.fai'
@@ -36,12 +35,11 @@ TE_table = pd.read_csv(g_bed.fn,header=None,usecols=[8],sep="\t")
 TE_table.rename(columns={8:'Gene'},inplace=True)
 
 for a in updown_stream:
-	b='gene_updown_'+str(a)+'bp'
-	s_bed = pbt.bedtool.BedTool.slop(g_bed,g=genome_file,l=a,r=a,s=True).saveas('s_bed.tmp')
-	mapping = pbt.bedtool.BedTool.intersect(s_bed,TEs,wa=True,wb=True)
-	m = pd.read_csv(mapping.fn,header=None,usecols=[8],sep="\t")
-	tmp = m[8].value_counts().to_dict()
-	TE_table[b] = TE_table['Gene'].map(tmp)
+b='gene_body'
+mapping = pbt.bedtool.BedTool.intersect(g_bed,TEs,wa=True,wb=True)
+m = pd.read_csv(mapping.fn,header=None,usecols=[8],sep="\t")
+tmp = m[8].value_counts().to_dict()
+TE_table[b] = TE_table['Gene'].map(tmp)
 
 for a in updown_stream:
 	b='up_'+str(a)+'bp'
