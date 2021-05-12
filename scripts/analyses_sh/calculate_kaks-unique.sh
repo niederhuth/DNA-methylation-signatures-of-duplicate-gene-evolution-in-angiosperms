ab289899b41fd39abadf3a52486d5fbf148d0026
Niederhuth <niederhu@msu.edu>
2021-05-11 22:50:16 -0400

    stuff

---
+++ scripts/analyses_sh/calculate_kaks-unique.sh
@@ -0,0 +1,33 @@
+#!/bin/bash --login
+#SBATCH --time=168:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=80GB
+#SBATCH --job-name calculate_KaKs
+#SBATCH --output=job_reports/%x-%j.SLURMout
+
+cd $PBS_O_WORKDIR
+sample=$(pwd | sed s/^.*\\///)
+SCRIPTS=$(pwd | sed s/angiosperms.*/angiosperms\\/scripts/)
+export PATH="$HOME/miniconda3/envs/gene-duplication/bin:$PATH"
+export PATH="$SCRIPTS/calculate_Ka_Ks_pipeline:$PATH"
+export LD_LIBRARY_PATH="$HOME/miniconda3/envs/gene-duplication/lib:$LD_LIBRARY_PATH"
+export PERL5LIB="$HOME/miniconda3/envs/gene-duplication/lib/perl5/site_perl/5.22.0"
+
+#Calculate KaKs
+cd dupgen/results-unique
+mkdir kaks_results
+for i in *pairs-unique
+do
+	echo "Calculating KaKs for each gene pair"
+	pairs=$(echo $i | tr '.' '\t' | cut -f2)
+	perl $SCRIPTS/calculate_Ka_Ks_pipeline/calculate_Ka_Ks_pipe.pl \
+		-d ../../ref/mcscanx/"$sample"-cds.fa \
+		-g "$i" \
+		-o kaks_results/"$sample"."$pairs".kaks \
+		-p $SCRIPTS/calculate_Ka_Ks_pipeline
+	sed -i '2d' kaks_results/"$sample"."$pairs".kaks
+done
+
+
