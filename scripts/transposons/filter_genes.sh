8ebcdec772d9f0d2c171dae6c39dcbbcebf74af6
Chad Niederhuth <niederhu@msu.com>
2022-06-11 15:37:20 -0400

    stuff

--- scripts/transposons/filter_genes.sh
+++ scripts/transposons/filter_genes.sh
@@ -126,5 +126,27 @@ python ${path2}/transposons/py/create_no_TE_genelist.py \
 	#Removed TEhmm against gypsy genes as this resulted in a high number of false positives
 	#--TEhmm gypsyHMM_analysis.out
 
+#Now combine with ortholog & methylation calls
+cat pfam_filtered_genes.txt blast_filtered_genes.txt gypsy_filtered_genes.txt | sort | uniq > filtered_genes.tsv
+fgrep -f filtered_genes.tsv ../mcscanx/${species}_orthogroups.tsv | sort -k1,1 > tmp
+if [ -f ../../methylpy/results/${species}_classified_genes.tsv ]
+then
+	fgrep -f filtered_genes.tsv ../../methylpy/results/${species}_classified_genes.tsv | cut -f1,30 | sort -k1,1 > tmp2
+	cut -f1 tmp2 > tmp3
+	fgrep -v -f tmp3 filtered_genes.tsv > tmp4
+	awk -v OFS="\t" '{print $1,"NA"}' tmp4 >> tmp2
+	sort -k1,1 tmp2 > tmp5
+	join -1 1 -2 1 tmp tmp5 | tr ' ' '\t' > filtered_genes.tsv
+else
+	mv tmp filtered_genes.tsv
+fi
+#Count the number & percentage of genes for filtered orthogroups 
+cut -f2 filtered_genes.tsv | sort | uniq > orthogroups.txt
+cut -f2 filtered_genes.tsv | sort | uniq -c | sed 's/^ *//' | tr ' ' '\t' | sort -k2,2 > tmp
+cut -f2 tmp > tmp2
+fgrep -f tmp2 ../mcscanx/${i}_orthogroups.tsv | cut -f2 | sort | uniq -c | sed 's/^ *//' | tr ' ' '\t' | sort -k2,2 > tmp3
+join -1 2 -2 2 tmp3 tmp | tr ' ' '\t' | awk -v OFS="\t" '{print $1,$2,$3,$3/$2}' > filtered_orthgroup_counts.tsv
+rm tmp*
+
 echo "Done"
 
